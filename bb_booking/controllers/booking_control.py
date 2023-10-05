ee4aa000b2f783d1d19c533b4a68090943d21b48
GianniTrezza <138105051+GianniTrezza@users.noreply.github.com>
2023-10-05 17:03:51 +0200

    Add files via upload

--- bb_booking/controllers/booking_control.py
+++ bb_booking/controllers/booking_control.py
@@ -92,8 +92,7 @@ class SoltHttpTest(models.Model):
     content = fields.Text('Content')
     response = fields.Text('Response')
 
-    
-# Caso 1: Assenza di filtri: questo caso vale anche per la presenza di filtri
+    # Caso 1: Assenza di filtri: questo caso vale anche per la presenza di filtri
     @api.multi
     def action_request_with_structure(self, content_data=None):
         for test in self:
@@ -104,9 +103,141 @@ class SoltHttpTest(models.Model):
             result = getattr(requests, test.method)(endpoint, json=content, auth=auth, headers=headers)
             test.write({'response': result.text})
 
+    
+
+
+# OPPURE
+    @api.multi
+    def add_accommodation(self, accommodation_data):
+        for test in self:
+            endpoint = "URL_OCTORATE/rest/accommodation" # Sostituisci con il tuo endpoint
+            # Esempio di endpoint = "https://api.octorate.example.com/rest/accommodation"
+            headers = {
+                'Content-Type': 'application/json', 
+                'Authorization': 'Basic YOUR_BASE64_CREDENTIALS', # Sostituisci con le tue credenziali base64
+                'key': 'YOUR_KEY' # Sostituisci con la tua chiave
+            }
+            
+            result = requests.post(endpoint, json=accommodation_data, headers=headers)
+            
+            if result.status_code == 200:
+                response_data = result.json()
+                if response_data.get("status") == "created":
+                    # Gestisci la risposta di successo, ad esempio salvando il codice della proprietà
+                    pass
+                else:
+                    # Gestisci gli errori di validazione o altri errori relativi ai dati
+                    pass
+            elif result.status_code in [401, 403]:
+                # Gestisci gli errori di autenticazione e autorizzazione
+                pass
+
+            test.write({'response': result.text})
+
+            # POSSIBILE FLOWCHART PER LA CREAZIONE DELL'API INTEGRANTE OCTORATE CON ODOO
+# 1) COME CREARE UN'API KEY DA DARE IN PASTO ALLA KEY DEGLI HEADERS
+# PREMESSA
+# Utenti della Chiave API (API attivate prima del 17-03-2020, Migrazione da Formato XML a JSON)
+# Per elaborare le richieste con la nuova API, dovresti seguire il metodo precedentemente menzionato.
+# Contattaci per ottenere le nuove credenziali; successivamente, potrai ottenere il token per le tue proprietà utilizzando il metodo Identity->migrateApiKey.
+
+# CREAZIONE API KEY DA OCTORATE
+# Per generare una chiave API da un servizio specifico (come Octorate, nel tuo caso), generalmente dovresti:
+
+# a)Accedere al tuo account sul sito web o piattaforma del servizio.
+# b)Navigare verso la sezione "Impostazioni", "Developers", "API", o una sezione simile, a seconda della piattaforma.
+# c)Qui, dovresti trovare un'opzione per generare una nuova chiave API o visualizzare le chiavi esistenti.
+# d)Segui le istruzioni fornite per ottenere la tua chiave
+
+
+# A) CONTATTA IL FORNITORE:Dovresti contattare Octorate per richiedere le nuove credenziali. 
+# Possono fornirti direttamente una nuova API key o darti accesso a un portale dove puoi generare tu stesso la chiave
+# B) Migrazione da XML a JSON: Una volta ottenute le nuove credenziali, sembra che ci sia un metodo chiamato migrateApiKey sotto una categoria chiamata Identity che dovresti utilizzare per ottenere un token per le tue proprietà. 
+# Questo potrebbe essere necessario se stavi utilizzando la vecchia API con formato XML e ora devi migrare a una nuova versione che utilizza JSON.
+# C) Utilizzo della Nuova API Key: Una volta ottenuta la nuova API key o il token, dovresti utilizzarla nelle tue richieste API come valore dell'header key.
+#  Assicurati di seguire le specifiche menzionate nella documentazione o nelle istruzioni fornite dal supporto di Octorate.
+# D)Testa la Nuova API: È sempre una buona pratica testare la nuova API in un ambiente sicuro (come un ambiente di test o di sviluppo) prima di usarla in produzione.
+#  Assicurati che tutto funzioni come previsto e che i dati vengano trasmessi nel formato corretto.
+
+
+# 2) COME CREARE UN API
+
+# A) Creazione stringa base 64 per autorizazione (esempio)
+
+# Username: myUsername
+# Password: myPassword
+
+import base64
+
+credentials = "myUsername:myPassword"
+base64_credentials = base64.b64encode(credentials.encode()).decode()
+print(base64_credentials)
+
+# esempio di base64_credentials: bXlVc2VybmFtZTpteVBhc3N3b3Jk
+# Esempio di key: abc1234defgh
+# B)
+# Headers model
+# headers = {
+#     'Authorization': 'Basic bXlVc2VybmFtZTpteVBhc3N3b3Jk', 
+#     'key': 'abc1234defgh', usare come key l'api key seguente le istruzioni del punto precedente
+#     'Content-Type': 'application/json'
+# }
+
+# C) Integrazione della sottostante funzione nel metodo add_accommodation(self, accommodation_data):
+import requests
+import json
+
+endpoint_accommodation = "https://api.octorate.example.com/rest/accommodation"
+headers_accommodation = {
+    "Content-Type": "application/json",
+    "Authorization": "Basic YOUR_BASE64_ENCODED_CREDENTIALS",
+    "key": "YOUR_API_KEY"
+}
+data_accommodation = {
+    "website": "www.mycompany.com",
+    "zipCode": "00199",
+    "city": "Roma",
+    "given_name": "Oscar",
+    "family_name": "Slim Dogg T",
+    "name": "Surf House Lisboa",
+    "latitude": 41.1212,
+    "longitude": 40.3232,
+    "email": "myname@mycompany.com",
+    "phone": "+393347608861",
+    "address": "Avenida da Liberdade 12",
+    "isoCountry": "IT",
+    "isoCurrency": "EUR"
+}
+
+response_accommodation = requests.post(endpoint_accommodation, json=data_accommodation, headers=headers_accommodation)
+accommodation_data = response_accommodation.json()
+# accommodation_id = accommodation_data.get('codice')[0]
+accommodation_id = accommodation_data.get('codice')
+
+endpoint_reservations = f"https://api.octorate.com/connect/reservations?accommodation_id={accommodation_id}"
+
+response_reservations = requests.get(endpoint_reservations)
+reservations_data = response_reservations.json()
+
+print(reservations_data)
+
+
+# 3) CREAZIONE DEL JSON CONTENENTE I RESERVATIONS_DATA 
+# import json
+
+# # Supponiamo che reservations_data sia il tuo dizionario
+# reservations_data = response_reservations.json()
+
+# # Scrivere i dati in un file JSON
+# with open('reservations_data.json', 'w') as json_file:
+#     json.dump(reservations_data, json_file, indent=4)
 
 
 
+
+
+
+# Uso di uno dei seguenti filtri
 # \"accommodation\":{\"address\":\"via filippo caruso\",\"checkinEnd\":20,\"checkinStart\":12,\"checkout\":12,\"city\":\"ROMA\",\"currency\":\"EUR\",\"id\":\"557782\",\"latitude\":41.8489657,\"longitude\":12.5764685,\"name\":\"OdooERP  Test Api Building L.A.\",\"phoneNumber\":\"+3906060606\",\"timeZone\":\"Europe/Rome\",\"timeZoneOffset\":\"+01:00\",\"zipCode\":\"00173\"}
 
 
--- bb_booking/models/booking_info.py
+++ bb_booking/models/booking_info.py
@@ -1,11 +1,10 @@
-from odoo import models, fields,_ , api
+from odoo import models, fields,_
 
 class roombooking(models.Model):
 
-    _inherit = "account.move"
+    _inherit = ["account.move"]
 
     # INFORMAZIONI GENERALI DELLA PRENOTAZIONE
-    # refer = fields.Char(string='ID Prenotazione')
     status = fields.Selection([
         ('pending', 'In approvazione'),
         ('confirmed', 'Confermato'),
@@ -22,11 +21,6 @@ class roombooking(models.Model):
     
     totalGross = fields.Float(string='Costo Totale')
     totalGuest = fields.Integer(string='Ospiti Totali')
-    
-    # ALTRE INFORMAZIONI DELLA PRENOTAZIONE
-    data_ingresso = fields.Date(string='Data di Ingresso', compute='_compute_date')
-    data_uscita = fields.Date(string="Data d'Uscita", compute='_compute_date')
-    # arrivalTime = fields.Char(string='Orario di Arrivo')
     channelName = fields.Char(string='Nome Canale di Prenotazione')
     currency = fields.Selection([
         ('eur', 'EUR'),
@@ -37,63 +31,71 @@ class roombooking(models.Model):
     booking_id = fields.Integer(string='ID Prenotazione')
     lastName = fields.Char(string='Cognome')
     paymentStatus = fields.Selection([
-        ('unpaid', 'Pagato'),
-        ('paid', 'Non Pagato'),
+        ('unpaid', 'Non Pagato'),
+        ('paid', 'Pagato'),
     ], string='Stato del Pagamento')
     
     paymentType = fields.Char(string='Metodo di Pagamento')
-    # product_id = fields.Many2one('product.product', string='Prodotto')
-    # rooms = fields.Integer(string='Numero di Stanze')
-    # roomName = fields.Char(string='Nome Stanza')
     totalChildren = fields.Integer(string='Totali Ragazzi')
     totalInfants = fields.Integer(string='Totali Neonati')
-    display_status = fields.Html(string='Stato visualizzato', compute='_compute_display_status', sanitize=False, store=False)
-    soggiorno_input = fields.Html(string='Soggiorno', compute='_compute_soggiorno_input', sanitize=False, store=False)
-
-    @api.depends('checkin', 'checkout')
-    def _compute_date(self):
-        for record in self:
-            record.data_ingresso = record.checkin.date() if record.checkin else False
-            record.data_uscita = record.checkout.date() if record.checkout else False
-
-    
-
-    @api.depends('status', 'paymentStatus')
-    def _compute_display_status(self):
-        for record in self:
-            stato_generico = dict(self._fields['status'].selection).get(record.status, "")
-            stato_pagamento = dict(self._fields['paymentStatus'].selection).get(record.paymentStatus, "")
-
-            combined = [
-                f"Stato: {stato_generico}" if stato_generico else "",
-                f"Stato Pagamento: {stato_pagamento}" if stato_pagamento else ""
-            ]
-
-            record.display_status = "<br/>".join(filter(None, combined))
-
-    @api.depends('data_ingresso', 'data_uscita')
-    def _compute_soggiorno_input(self):
-        for record in self:
-            if record.data_ingresso and record.data_uscita:
-                intervallo = (record.data_uscita - record.data_ingresso).days + 1
-                record.soggiorno_input = f"Ingresso: {record.data_ingresso} <br/> Uscita: {record.data_uscita} <br/> Permanenza: {intervallo} giorni"
-            else:
-                record.soggiorno_input = ""
 
 class prenotadettagli(models.Model):
-
     _inherit = "account.move.line"
 
-    roomName = fields.Many2one ('product.product', string='Nome Stanza')
-    channelNotes = fields.Text(string='Note del Canale')
-    data_ingresso = fields.Date(string='Data di Ingresso', compute='_compute_date')
-    data_uscita = fields.Date(string="Data d'Uscita", compute='_compute_date')
-    rooms = fields.Integer(string='Numero di Stanze')
+    # roomName = fields.Many2one ('product.product', string='Nome Stanza')
+    # rooms = fields.Float(string='Numero di Stanze')
     
-    roomGross = fields.Float(string='Costo Stanza')
-    touristTax = fields.Float(string='Tassa Turistica')
+
+
     # tassa di soggiorno= (creare una funzione che prenda come campi numero infanti, neonati, che calcoli il numero totale di ospiti e che moltiplichi il totale per 2 euro
-    totalPaid = fields.Float(string='Totale Pagato')
+
+# class otherproperties(models.Model):
+#     _inherit = ["utm.campaign"]
+
+#     otherproperties_id = fields.Many2one('otherproperties', 'Related Property')
+
+
+
+# Altre features e funzioni
+
+    
+# arrivalTime = fields.Char(string='Orario di Arrivo')
+# data_ingresso = fields.Date(string='Data di Ingresso', compute='_compute_date')
+# data_uscita = fields.Date(string="Data d'Uscita", compute='_compute_date')
+
+# display_status = fields.Html(string='Stato visualizzato', compute='_compute_display_status', sanitize=False, store=False)
+    # soggiorno_input = fields.Html(string='Soggiorno', compute='_compute_soggiorno_input', sanitize=False, store=False)
+
+    # @api.depends('checkin', 'checkout')
+    # def _compute_date(self):
+    #     for record in self:
+    #         record.data_ingresso = record.checkin.date() if record.checkin else False
+    #         record.data_uscita = record.checkout.date() if record.checkout else False
+
+    
+
+    # @api.depends('status', 'paymentStatus')
+    # def _compute_display_status(self):
+    #     for record in self:
+    #         stato_generico = dict(self._fields['status'].selection).get(record.status, "")
+    #         stato_pagamento = dict(self._fields['paymentStatus'].selection).get(record.paymentStatus, "")
+
+    #         combined = [
+    #             f"Stato: {stato_generico}" if stato_generico else "",
+    #             f"Stato Pagamento: {stato_pagamento}" if stato_pagamento else ""
+    #         ]
+
+    #         record.display_status = "<br/>".join(filter(None, combined))
+
+    # @api.depends('data_ingresso', 'data_uscita')
+    # def _compute_soggiorno_input(self):
+    #     for record in self:
+    #         if record.data_ingresso and record.data_uscita:
+    #             intervallo = (record.data_uscita - record.data_ingresso).days + 1
+    #             record.soggiorno_input = f"Ingresso: {record.data_ingresso} <br/> Uscita: {record.data_uscita} <br/> Permanenza: {intervallo} giorni"
+    #         else:
+    #             record.soggiorno_input = ""
+
 
     
 
