5609d7f536388dba4cef2be301b2e1ff23e9a494
GianniTrezza <138105051+GianniTrezza@users.noreply.github.com>
2024-01-04 10:55:48 +0100

    Add files via upload

--- infratel_sale_ext/models/sale_order.py
+++ infratel_sale_ext/models/sale_order.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+import logging
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -57,11 +59,17 @@ class SaleOrder(models.Model):
         return True
 
     def create_purchase_order_action(self):
-        # self.ensure_one()
         if self.state != 'sale':
             raise UserError("L'ODV deve essere confermato per generare un ODA.")
-        
-        # return True
+        if not self.partner_id:
+            raise UserError("Devi selezionare un cliente prima di creare un ordine di acquisto.")
+        _logger.info(f"Creazione ODA per Sale Order {self.name} con partner_id: {self.partner_id.id}")
+
+
+        partner = self.env['res.partner'].browse(self.partner_id.id)
+        if not partner.exists():
+            raise UserError("Fornitore non valido o non esistente per l'ODV.")
+
         return {
             'name': 'Seleziona Fornitore',
             'type': 'ir.actions.act_window',
@@ -69,12 +77,6 @@ class SaleOrder(models.Model):
             'view_mode': 'form',
             'target': 'new',
             'context': {
-                'default_partner_id': self.partner_id.id,
-                'default_origin': self.name,
-                'default_project_request_id': self.project_request_id.id,
-                'default_infr_order': self.infr_order,
-                'default_framework_agreement_id': self.framework_agreement_id.id,
-
             },
         }
 
@@ -92,21 +94,23 @@ class SaleOrderLine(models.Model):
     route_length_rel = fields.Char(related='plant_route_id.route_length', string="Lunghezza tratta")
     iru_duration_id = fields.Many2one('iru.duration', string="Durata IRU")
 
-
-# # Generazione del wizard legato al tasto genera ODA (parte di sviluppo)
-
+# ***********************GENERAZIONE DEL WIZARD LEGATO AL TASTO "CREAZIONE ODA" (SVILUPPO)****************************
+#*******************Filtraggio dei soli fornitori e degli accordi quadro ancora attivi ad essi correlati***********************
 class FornitoreWizard(models.TransientModel):
     _name = 'fornitori.wizard'
     _description = 'Selezione di un fornitore per l\'ODA'
 
     fornitore_id = fields.Many2one('res.partner', string='Fornitore', domain=[('infr_contact_type', '=', 'fornitore')])
-    # accordo_quadro_id = fields.Many2one('framework.agreement', string='Accordi Quadro Attivi (prova)')
-    accordo_quadro_id = fields.Many2one('framework.agreement', string='Accordo Quadro (new)', domain="[('partner_id', '=', fornitore_id), ('is_state_valid', '=', True)]")
+    accordo_quadro_id = fields.Many2one('framework.agreement', string='Accordi Quadro Attivi', domain="[('partner_id', '=', fornitore_id), ('is_state_valid', '=', True)]")
     
     def conferma_selezione_accordo_quadro(self):
         self.ensure_one()
         if not self.accordo_quadro_id:
             raise UserError('Seleziona un accordo quadro.')
+        if not self.fornitore_id:
+            raise UserError('Seleziona un fornitore prima di procedere.')
+
+        _logger.info(f"Fornitore selezionato ID: {self.fornitore_id.id}")
         
         return {
             'name': 'Seleziona Prodotti',
@@ -114,48 +118,42 @@ class FornitoreWizard(models.TransientModel):
             'res_model': 'prodotti.lista',
             'view_mode': 'form',
             'target': 'new',
+            'context': {'default_partner_id': self.fornitore_id.id},
         }
-
+#**********************Filtraggio dei soli fornitori e degli accordi quadro ancora attivi ad essi correlati*****************
 class ListaProdotti(models.TransientModel):
     _name = 'prodotti.lista'
     _description = 'Selezione dei prodotti per l\'ODA'
     
-    product_ids = fields.Many2many('product.product', string='Prodotti')
+    product_ids = fields.Many2many('product.product', string='Prodotti disponibili')
+    fornitore_id = fields.Many2one('res.partner', string='Fornitore', domain=[('infr_contact_type', '=', 'fornitore')])
     
     def conferma_selezione_prodotti(self):
         self.ensure_one()
         if not self.product_ids:
             raise UserError('Scegli almeno un prodotto.')
+        if not self.env.context.get('default_partner_id'):
+            raise UserError('ID del fornitore non trovato nel contesto.')
         
-        context = self.env.context
-        partner_id = context.get('default_partner_id')
-        if not partner_id:
-            raise UserError('ID del fornitore mancante nel contesto.')
+        purchase_order = self.env['purchase.order'].create({
+            'partner_id': self.env.context['default_partner_id'],
+            'origin': self._context.get('origin'),
+            'project_request_id': self._context.get('project_request_id'),
+            'infr_order': self._context.get('infr_order'),
+            'framework_agreement_id': self._context.get('framework_agreement_id')
 
-        purchase_order_vals = {
-            'partner_id': partner_id,
-            'origin': context.get('default_origin'),
-            'project_request_id': context.get('default_project_request_id'),
-            'infr_order': context.get('default_infr_order'),
-            'framework_agreement_id': context.get('default_framework_agreement_id')
-            }
-        
-        purchase_order = self.env['purchase.order'].create(purchase_order_vals)
+        })
+        for product in self.product_ids:
+            self.env['purchase.order.line'].create({
+                'order_id': purchase_order.id,
+                'product_id': product.id,
+                'date_planned': fields.Datetime.now(),
+            })
 
-        
-        return purchase_order
-
-#     def conferma_selezione_fornitore(self):
-#         self.ensure_one()
-#         if not self.fornitore_id:
-#             raise UserError('Seleziona un fornitore.')
-#         if not self.fornitore_id.framework_agreement_count:
-#             raise UserError('Il fornitore selezionato non ha accordi quadro attivi.')
-
-#         return {
-#             'name': 'Seleziona Prodotti',
-#             'type': 'ir.actions.act_window',
-#             'res_model': 'prodotti.lista',
-#             'view_mode': 'form',
-#             'target': 'new',
-#         }
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Ordine di Acquisto Creato',
+            'res_model': 'purchase.order',
+            'res_id': purchase_order.id,
+            'view_mode': 'form',
+        }
