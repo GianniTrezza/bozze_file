e4e02a0625b5af8588b428109cbaf5097cd81dfc
GianniTrezza <138105051+GianniTrezza@users.noreply.github.com>
2024-01-09 09:51:03 +0100

    Add files via upload

--- infratel_sale_bar_ext/models/status_bar_sale.py
+++ infratel_sale_bar_ext/models/status_bar_sale.py
@@ -1,55 +1,48 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    
-    state = fields.Selection(selection_add=[
-            ('sent_for_validation', 'In attesa di validazione'),
-            ('sent_for_approval', 'In attesa di approvazione'),
-            ('signed', 'Firmato')
-        ])
-    button_validation_visible = fields.Boolean(compute='_compute_button_visibility')
-    button_approval_visible = fields.Boolean(compute='_compute_button_visibility')
-    button_sign_visible = fields.Boolean(compute='_compute_button_visibility')
 
-    # Logiche per la visibilità dei bottoni
-    @api.depends('state', 'user_id')
+    state = fields.Selection(selection_add=[
+        ('sent_for_validation', 'In attesa di validazione'),
+        ('sent_for_approval', 'In attesa di approvazione'),
+        ('signed', 'Firmato')
+    ])
+
+    # Campi calcolati per la visibilità dei pulsanti
+    show_button_validation = fields.Boolean(compute='_compute_button_visibility')
+    show_button_approval = fields.Boolean(compute='_compute_button_visibility')
+    show_button_sign = fields.Boolean(compute='_compute_button_visibility')
+
+    @api.depends('state')
     def _compute_button_visibility(self):
         for record in self:
-            user = self.env.user
-            # Qui inserisci la logica per definire se l'utente è un Business Developer o DDI
-            is_business_developer = user.has_group('infratel_sale_bar_ext.group_business_developer_id')
-            # is_ddi = user.has_group('your_module.group_ddi')
-
-            record.button_validation_visible = record.state == 'draft'
-            record.button_approval_visible = record.state == 'sent_for_validation' and is_business_developer
-            record.button_sign_visible = record.state == 'sent_for_approval' 
-            # Aggiungere a questo livello il is_ddi
+            record.show_button_validation = record.state == 'draft'
+            record.show_button_approval = record.state == 'sent_for_validation'
+            record.show_button_sign = record.state == 'sent_for_approval'
 
     # Azione "Invia per validazione"
     def action_send_for_validation(self):
         self.ensure_one()
         if self.state != 'draft':
-            raise UserError("Il preventivo è in attesa d'approvazione da parte del Business Developer.")
+            raise UserError("Il preventivo è in attesa di validazione da parte del Business Developer.")
         self.state = 'sent_for_validation'
         
-
     # Azione "Invia per approvazione"
     def action_send_for_approval(self):
         self.ensure_one()
         if self.state != 'sent_for_validation':
-            raise UserError("Il preventivo è stato approvato: si attenda la firma e l'invio da parte del DDI.")
+            raise UserError("Il preventivo è in attesa di approvazione da parte dell'utente DDI.")
         self.state = 'sent_for_approval'
 
-#***************************** Pulsante "Invia per approvazione" e stato "Firma"*************************
-
+    # Pulsante "Invia per approvazione" e stato "Firma"
     def action_sign(self):
-        # if self.state == 'sent_for_approval':
-        #     self.state = 'signed'
         self.ensure_one()
         if self.state != 'sent_for_approval':
-            raise UserError("Preventivo firmato ed inviato correttamente da parte del DDI.")
+            raise UserError("Preventivo firmato correttamente da parte del DDI.")
         self.write({'state': 'signed'})
+
 
 # ********************************* ACCESSO UTENTI*************************************************
     # def is_user_business_developer(self):
